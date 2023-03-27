c48fd8c5e18d7a9d12c2a75b211242444b97b73e
SHAIK MUDSIR ALI <126944337+ShaikMudsirAli1@users.noreply.github.com>
2023-03-27 09:58:42 +0530

    First_ Commit

---
+++ intro- to- node/index.js
@@ -0,0 +1,17 @@
+//var superheroes = require("superheroes"); //reuire the module superheroes
+
+// ^^^^^^^ External npm Package superheroes ^^^^^^^^
+// var mySuperName = superheroes.random();
+// console.log(mySuperName);
+
+// var supervillains = require("supervillains");
+
+// var mySupervillainsNames = supervillains.random();
+
+// console.log(mySupervillainsNames);
+
+var catnamer = require("cat-namer");
+
+var myCatName = catnamer.random();
+
+console.log(myCatName);
